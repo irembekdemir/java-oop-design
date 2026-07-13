2b83496045e24151ab44fef8e1970b21beb0e1ea
irem <irembekdemir@hacettepe.edu.tr>
2026-07-13 17:30:32 +0300

    Update HeavyCargo.java

--- cargo-firm/HeavyCargo.java
+++ cargo-firm/HeavyCargo.java
@@ -1,15 +1,15 @@
 public class HeavyCargo extends Cargo {
-    private static final double BASE_RATE = 2.5; // Named Constant
+    private static final double BASE_RATE = 2.5; 
     private static final double EXTRA_FEE = 150.0;
 
     public HeavyCargo(String trackingNumber, double weight, int distanceKm) {
-        super(trackingNumber, weight, distanceKm); // Üst sınıfın constructor'ını çağırır
+        super(trackingNumber, weight, distanceKm); 
     }
 
     @Override
     public double calculatePrice() {
         double price = getWeight() * BASE_RATE + (getDistanceKm() * 0.5);
-        if (getWeight() > 30) { // 30 kg üstü ağır yüke girer
+        if (getWeight() > 30) { 
             price += EXTRA_FEE;
         }
         return price;
